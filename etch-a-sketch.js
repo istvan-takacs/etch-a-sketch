64781a6a6067fd3483edfa5bd5472240b02e1ff4
istvantakacs <91099626+istvan-takacs@users.noreply.github.com>
2025-10-21 23:23:35 +0100

    Add Range Input slider to GUI
    Add slider to change the grid size of the
    etch-a-sketch display. User can now control the
    "resolution" of the board.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,18 +1,15 @@
-const squareContainer = document.querySelector(".square-container")
+let gridSize = 16;
 let mouseDown = false;
-document.addEventListener("mousedown", () => {
-    mouseDown = true;
-})
-document.addEventListener("mouseup", () => {
-    mouseDown = false;
-})
 
-function createDisplay() {
-    for (let i = 0; i < 16; i++) {
+
+function createDisplay(size=gridSize) {
+    const squareContainer = document.querySelector(".square-container")
+    squareContainer.innerHTML = "";
+    for (let i = 0; i < size; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
         squareContainer.appendChild(row);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < size; j++) {
             let square = document.createElement("div");
             square.classList.add("square");
             colorSquare(square);
@@ -22,6 +19,13 @@ function createDisplay() {
 }
 
 function colorSquare(square) {
+    document.addEventListener("mousedown", () => {
+        mouseDown = true;
+    })
+    document.addEventListener("mouseup", () => {
+        mouseDown = false;
+    })
+
     square.addEventListener("mouseenter", () => {
         if (mouseDown) {
             square.style["background-color"] = "black";
@@ -32,5 +36,26 @@ function colorSquare(square) {
     })
 }
 
+function manipulateGridBySlider() {
+    let slider = document.getElementById("gridSize");
+    let buttonContainer = document.querySelector(".button-container");
+    let sliderOutput = document.createElement("p");
+    sliderOutput.textContent = slider.value;
+    buttonContainer.appendChild(sliderOutput);
 
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.addEventListener("mouseup", function() {
+        sliderOutput.textContent = this.value;
+        createDisplay(size=this.value)
+    })
+    slider.addEventListener("mousemove", function() {
+        if (mouseDown) {
+            sliderOutput.textContent = this.value;
+        }
+    })
+}
+
+
+manipulateGridBySlider()
 createDisplay()
