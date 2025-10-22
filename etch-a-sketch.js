ae1f2b2822005092c36a673395b9745ad37202a5
istvantakacs <91099626+istvan-takacs@users.noreply.github.com>
2025-10-22 22:02:23 +0100

    Add clear button, palette, and better UI
    Fix minor issues to improve performance.
    Add features such as clear button to clear the
    screen and the palette so that colours can be
    changed.
    Significantly improve UI, by adding custom element
    appearances, a blue-purple linear gradient theme,
    more responsive buttons etc.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,11 @@
-let gridSize = 16;
 let mouseDown = false;
+let squareColor = "black";
+
+document.addEventListener("mousedown", () => mouseDown = true);
+document.addEventListener("mouseup", () => mouseDown = false);
 
 
-function createDisplay(size=gridSize) {
+function createDisplay(size) {
     const squareContainer = document.querySelector(".square-container")
     squareContainer.innerHTML = "";
     for (let i = 0; i < size; i++) {
@@ -12,50 +15,80 @@ function createDisplay(size=gridSize) {
         for (let j = 0; j < size; j++) {
             let square = document.createElement("div");
             square.classList.add("square");
-            colorSquare(square);
+            colorSquare(square, squareColor);
             row.appendChild(square);
         } 
     }
 }
 
 function colorSquare(square) {
-    document.addEventListener("mousedown", () => {
-        mouseDown = true;
-    })
-    document.addEventListener("mouseup", () => {
-        mouseDown = false;
-    })
-
     square.addEventListener("mouseenter", () => {
         if (mouseDown) {
-            square.style["background-color"] = "black";
+            square.style.backgroundColor = squareColor;
         }
     })
     square.addEventListener("mousedown", () => {
-        square.style["background-color"] = "black";
+        square.style.backgroundColor = squareColor;
     })
 }
 
 function manipulateGridBySlider() {
     let slider = document.getElementById("gridSize");
-    let buttonContainer = document.querySelector(".button-container");
+    let slideContainer = document.querySelector(".slidecontainer")
+    // let buttonContainer = document.querySelector(".button-container");
     let sliderOutput = document.createElement("p");
-    sliderOutput.textContent = slider.value;
-    buttonContainer.appendChild(sliderOutput);
+    sliderOutput.setAttribute("id", "gridSizeText");
+    sliderOutput.textContent = slider.value + " x " + slider.value;
+    slideContainer.appendChild(sliderOutput);
 
 
     // Update the current slider value (each time you drag the slider handle)
-    slider.addEventListener("mouseup", function() {
-        sliderOutput.textContent = this.value;
-        createDisplay(size=this.value)
+    slider.addEventListener("input", function() {
+    sliderOutput.textContent = this.value;
+    });
+
+    slider.addEventListener("change", function() {
+        createDisplay(this.value);
+    });
+
+}
+
+function addClearButton() {
+    let buttonContainer = document.querySelector(".button-container");
+    let clearButton = document.createElement("button");
+    clearButton.setAttribute("class", "clear-button")
+    clearButton.textContent = "Clear";
+    clearButton.addEventListener("click", function() {
+        let size = document.querySelector("#gridSizeText").textContent
+        createDisplay(size);
     })
-    slider.addEventListener("mousemove", function() {
-        if (mouseDown) {
-            sliderOutput.textContent = this.value;
-        }
-    })
+    buttonContainer.appendChild(clearButton);
+}
+
+function addPalette() {
+    let buttonContainer = document.querySelector(".button-container");
+    let paletteContainer = document.createElement("div");
+    paletteContainer.setAttribute("id", "palette");
+    buttonContainer.appendChild(paletteContainer);
+    let paletteDesc = document.createElement("h2");
+    paletteDesc.textContent = "Palette";
+    paletteContainer.appendChild(paletteDesc);
+    let paletteButtonContainer =  document.createElement("div");
+    paletteContainer.appendChild(paletteButtonContainer);
+
+    let arr = ["black", "red", "orange", "yellow", "green", "blue", "purple", "pink"];
+    arr.forEach(color => {
+        let colorButton = document.createElement("button");
+        colorButton.style.backgroundColor = color;
+        colorButton.addEventListener("click", function() {
+            squareColor = color;
+        })
+        paletteButtonContainer.appendChild(colorButton); 
+    });
 }
 
 
 manipulateGridBySlider()
-createDisplay()
+createDisplay(16)
+addClearButton()
+addPalette()
